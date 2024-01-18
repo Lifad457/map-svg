4e6850537750d1550fb0f53c1ff5bdc6fce37b88
Lifad457 <cedric.bison@gmail.com>
2024-01-18 07:08:26 +0100

    tests

--- src/App.css.js
+++ src/App.css.js
@@ -3,51 +3,57 @@ import styled from "styled-components";
 export const StyledMap = styled.svg`
     display: block;
     position: absolute;
-    top: -7%;
-    left: 3%;
-    width: 120%;
-    rotate: -12deg;
+    top: -25.9rem;
+    left: 4rem;
+    margin-left: .5rem;
+    rotate: -9deg;
 
     #outlines {
-        stroke: none;
+        stroke: chocolate;
     }
     .state {
-        fill: rgb(228, 180, 180);
+        fill: #e4b4b4;
     }
 `
 export const StyledMapContainer = styled.div`
     display: inline-block;
     position: relative;
-    width: 100vh;
-    padding-bottom: 100vh;
-    vertical-align: middle;
-    background-color: bisque;
+    width: 700px;
+    height: 600px;
+    background: rgb(14,33,204);
+    background: linear-gradient(45deg, rgba(14,33,204,1) 60%, rgba(228,180,180,1) 100%);
+    font-family: var(--ff-primary);
+    margin: 3rem;
+    overflow: hidden;
 
-    .sf-la {
+    /* .sf-la {
         position: absolute;
-        top: 50%;
-        left: 10%;
-        width: 5%;
-        height: 10%;
+        top: 10rem;
+        left: 10rem;
+        width: 20rem;
+        height: 20rem;
         border-top-left-radius: 50%;
         border-bottom-left-radius: 50%;
         border: 2px solid red;
         border-right: 0;
         border-top: 0;
         border-style: dashed;
-        z-index: 1;
-    }
+    } */
 
     .LA {
         color: red;
         position: absolute;
-        top: 82.5%;
-        left: 26%;
-        font-size: 2rem;
+        top: 31rem;
+        left: 14rem;
+        font-size: clamp(1rem, 5vw + .5rem, 1.5rem);
+        z-index: 1;
     }
+
     .LA p {
         translate: -3rem;
-        line-height: 1.6rem;
+        font-size: clamp(1rem, 5vw + .5rem, 1.5rem);
+        line-height: 1.9rem;
         text-align: center;
+        text-shadow: .1rem .1rem 2px white;
     }
 `
--- src/Global.css.js
+++ src/Global.css.js
@@ -6,5 +6,14 @@ const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
     }
+
+    &:root {
+        --ff-primary: 'Lemon', sans-serif;
+    }
+    /* font-size: clamp(2rem, 5vw + .5rem, 3rem);
+    font-size: clamp(1rem, 5vw + .5rem, 1.5rem); 
+    display: grid;
+    gap: min(2rem, 5vw);
+    */
 `
 export default GlobalStyle
